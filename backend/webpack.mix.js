74a44ed8b8f00c2b5d9f7c79af80946205d0dd24
shonansurvivors <shonansurvivors@gmail.com>
2021-06-27 11:41:02 +0900

    version assets

--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -15,4 +15,4 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-]);
+]).version();
